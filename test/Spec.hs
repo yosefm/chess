4cf4fdd41625a35797d6bbcc06239e412d96467c
Yosef Meller <yosefm@gmail.com>
2025-05-03 11:16:43 +0300

    Start testing: valid moves

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,16 @@
+import qualified Data.Set as Set
+
+import Lib (validMoves, startBoard)
+import Test.Hspec
+
+equalAsSets :: (Eq a, Ord a) => [a] -> [a] -> Bool
+equalAsSets a b = Set.fromList a == Set.fromList b
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "Valid moves" $ do
+        it "lets white peon move forward to empty squares" $ do
+            validMoves startBoard (1,1) `shouldSatisfy` equalAsSets [(2,1), (3,1)]
+        it "lets black peon move forward to empty squares" $ do
+            validMoves startBoard (6,1) `shouldSatisfy` equalAsSets [(4,1), (5,1)]
+
